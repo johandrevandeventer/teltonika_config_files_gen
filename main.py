9f9d57d7a1080a2071960ef49fadb03c093f824f
Johandre van Deventer <johandrevdeventer@gmail.com>
2025-06-12 09:22:26 +0200

    add functionality to retrieve and create configuration files from device details

--- main.py
+++ main.py
@@ -14,6 +14,16 @@ def main():
 
     df = data.fix_building_names(df)
 
+    original_config_file_path = files.get_original_config_path(
+        config.ORIGINAL_CONFIG_FILE_NAME
+    )
+
+    config_files_path = files.get_config_files_path("config_files")
+
+    print("config_files_path:", config_files_path)
+
+    files.create_config_files(df, config_files_path, original_config_file_path)
+
 
 if __name__ == "__main__":
     main()
