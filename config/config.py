b5300b334941ac478291ba1b28c12b813dcd364c
Johandre van Deventer <johandrevdeventer@gmail.com>
2025-06-12 09:22:16 +0200

    add configuration constants for original config file and firmware version

--- config/config.py
+++ config/config.py
@@ -1,3 +1,5 @@
 """Configuration file for the application."""
 
 DEVICES_DETAILS_FILE_NAME = "devices_details.xlsx"
+ORIGINAL_CONFIG_FILE_NAME = "generic_backup.tar.gz"
+FW_VERSION = "FW_00_07_08_2"
